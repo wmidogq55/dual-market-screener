00f9161e12624178ad2d321ea75036c773bf303d
wmidogq55 <wmidogq55@gmail.com>
2025-04-05 22:25:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from FinMind.data import DataLoader
+from FinMind import DataLoader
 import ta
 from datetime import datetime, timedelta
 
