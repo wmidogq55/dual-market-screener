293432901d04253ac5cc3431a4bf08b7fcb7e83c
wmidogq55 <wmidogq55@gmail.com>
2025-04-05 22:19:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from datetime import datetime, timedelta
 
 # === 使用者登入（改用帳號密碼）===
 api = DataLoader()
-api.login(user_id="wmidogq55", password="single0829")  # ← 換成你的帳號密碼
+api.login(token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJkYXRlIjoiMjAyNS0wNC0wNSAyMTozMTo1NyIsInVzZXJfaWQiOiJ3bWlkb2dxNTUiLCJpcCI6IjExMS4yNDYuODIuMjE1In0.EMBmMMyYExvSqI1le-2DCTmOudEhrzBRqqfz_ArAucg")
 
 # === 日期區間設定 ===
 end_date = datetime.today()
